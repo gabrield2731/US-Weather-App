4d1788a6262aa7b71156fecaff03f7c66d18515e
Gabriel Dong <gabrielhdong@gmail.com>
2022-09-28 18:05:24 -0500

    adding all files

---
+++ script.js
@@ -0,0 +1,53 @@
+const API_KEY = "9bc078eb0381c5c9658ed6088c236bf9";
+
+// get weather data from specific city
+async function getWeather(city) {
+  const response = await fetch(
+    "http://api.openweathermap.org/data/2.5/weather?q=" +
+      city +
+      ",us&APPID=" +
+      API_KEY
+  );
+
+  const data = await response.json();
+
+  displayWeather(data);
+}
+
+// displays weather
+function displayWeather(data) {
+  const { name } = data;
+  const { icon, description } = data.weather[0];
+  const { temp, humidity, feels_like } = data.main;
+  const { speed } = data.wind;
+
+  document.querySelector(".city").innerText = "Weather in: " + name;
+  document.querySelector(".weather__icon").src =
+    "http://openweathermap.org/img/wn/" + icon + ".png";
+  document.querySelector(".temp").innerText =
+    Math.round(1.8 * (temp - 273) + 32) + "°F";
+  document.querySelector(".description").innerText = description;
+  document.querySelector(".wind").innerText = "Wind Speed: " + speed + " mph";
+  document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
+  document.querySelector(".feels_like").innerText =
+    "Feels like: " + Math.round(1.8 * (feels_like - 273) + 32) + "°F";
+}
+
+// get the city that the user inputs
+function searchWeather() {
+  const city = document.querySelector(".search__bar").value;
+  getWeather(city);
+}
+
+document.querySelector(".search__bar").addEventListener("keyup", (event) => {
+  if (event.key == "Enter") {
+    searchWeather();
+  }
+});
+
+// Listen for click event on search button
+document
+  .querySelector(".search__button")
+  .addEventListener("click", () => searchWeather());
+
+getWeather("Nashville");
